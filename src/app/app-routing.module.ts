db2a878b1f3c16f6a37284da035696183ffd42f2
Romain Hubert <rom.hubertcb@gmail.com>
2020-10-11 18:46:30 +0200

    DAS-12: Add CRUD capabilities to My Account (Spendings) app - Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SpendingListComponent} from "./spending-list/spending-list.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'spending-list', component: SpendingListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpendingListComponent } from './spending-list/spending-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {NgxPaginationModule} from "ngx-pagination";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpendingListComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    NgxPaginationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
